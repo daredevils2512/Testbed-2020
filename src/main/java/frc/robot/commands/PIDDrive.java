ddfe5da95fc21ee882c14dbbeb933c26b51feb0d
Toby <tmkoth431@isd709.org>
2020-01-23 19:59:50 -0600

    still need to tune PID

--- src/main/java/frc/robot/commands/PIDDrive.java
+++ src/main/java/frc/robot/commands/PIDDrive.java
@@ -8,20 +8,22 @@ public class PIDDrive extends CommandBase {
   private final PIDdrivetrain m_PIDdrivetrain;
   private double m_leftCurrentPos;
   private double m_rightCurrentPos;
-  private final int kPointOffset = 12;
   private double m_forward;
 
-  public PIDDrive(double forward) {
+  public PIDDrive(PIDdrivetrain PIDdrivetrain, double forward) {
     m_forward = forward;
-    m_PIDdrivetrain = new PIDdrivetrain();
+    m_PIDdrivetrain = PIDdrivetrain;
     m_leftCurrentPos = SmartDashboard.getNumber("left inches", 0.0);
     m_rightCurrentPos = SmartDashboard.getNumber("right inches", 0.0);
   }
 
   @Override
   public void execute() {
-    m_PIDdrivetrain.setSetPoint(m_leftCurrentPos + (m_forward * kPointOffset), m_rightCurrentPos + (m_forward * kPointOffset));
+    System.out.println("dufhkncsajmfdkxaxfskjdahfkdsaljomfciahlmfxkajhfcomiahef,xljahfcoimauhlfexakuhfncauesgfiurehfcakufehal");
+    m_PIDdrivetrain.setSetPoint(m_forward + m_leftCurrentPos, m_forward + m_rightCurrentPos);
   }
+
+  
 
   @Override
   public boolean isFinished() {
