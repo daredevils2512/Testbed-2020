181de0ebdb52edfc1293111dd803d614cd60ab01
Kobi Jones <kobijonesmail@gmail.com>
2020-01-31 18:32:16 -0600

    Added some more conversion functions

--- src/main/java/frc/robot/utils/Conversions.java
+++ src/main/java/frc/robot/utils/Conversions.java
@@ -8,26 +8,42 @@
 package frc.robot.utils;
 
 /**
- * Add your docs here.
+ * Helper class for unit converions
  */
 public final class Conversions {
-
-    public static final int inchesPerFoot = 12;
-    public static final double cmPerInch = 2.54;
-    public static final double cmPerMeter = 100;
+    public static final int INCHES_PER_FOOT = 12;
+    public static final double INCHES_PER_METER = 0.0254;
+    public static final double METERS_PER_INCH = 39.3701;
+    public static final double CENTIMETERS_PER_INCH = 2.54;
+    public static final int CENTIMETERS_PER_METER = 100;
+    public static final double FEET_PER_METER = 3.28084;
+    public static final double METERS_PER_FOOT = 0.3048;
 
     private Conversions() {
+
     }
 
-    public static double inchesTocm(double inches) {
-        return inches * cmPerInch;
+    public static double inchesToCentimeters(double inches) {
+        return inches * CENTIMETERS_PER_INCH;
     }
 
-    public static double feetInchesToInches(int feet, double inches) {
-        return (feet * inchesPerFoot) + inches;
+    public static double feetToInches(double feet) {
+        return feet * INCHES_PER_FOOT;
+    }
+
+    public static double feetToMeters(double feet) {
+        return feet * METERS_PER_FOOT;
+    }
+
+    public static double feetAndInchesToInches(int feet, double inches) {
+        return feetToInches(feet) + inches;
+    }
+
+    public static double feetAndInchesToMeters(int feet, double inches) {
+        return feetToMeters(feet) + inchesToMeters(inches);
     }
 
     public static double inchesToMeters(double inches) {
-        return inchesTocm(inches) / cmPerMeter;
+        return inches * METERS_PER_INCH;
     }
 }
