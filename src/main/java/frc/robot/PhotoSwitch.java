6cacbe98113a899564fc0da7aea749a359abe98d
NOAH <smurfmuchies@gmail.com>
2020-02-15 07:58:35 -0600

    Photo switch

---
+++ src/main/java/frc/robot/PhotoSwitch.java
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+/**
+ * Add your docs here.
+ */
+public class PhotoSwitch {
+  private final DigitalInput m_photoSwitch = new DigitalInput(0);
+
+
+  public boolean getDetected() {
+    return m_photoSwitch.get();
+  }
+}
